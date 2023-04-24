5e778a7d3fc1facc607ad06b7663a1169efcabf5
Khalelalsam <anasalhomseee@gmail.com>
2023-04-24 07:39:57 +0000

    sadas

--- tcp_client.go
+++ tcp_client.go
@@ -4,25 +4,29 @@ import (
 	"net"
 	"log"
 	"os"
+"github.com/Khalelalsam/is105sem03/mycrypt"
 )
 
 func main() {
-	conn, err := net.Dial("tcp", "127.0.0.1:")
+	if len(os.Args) < 2 {
+		log.Fatal("Usage: tcp_client <message>")
+	}
+kryptertMelding := mycrypt.Krypter([]rune(os.Args[1]), mycrypt.ALF_SEM03, 4)
+	log.Println("Kryptert melding: ", string(kryptertMelding))
+conn, err := net.Dial("tcp", "172.17.0.2:8080")
 	if err != nil {
 		log.Fatal(err)
 	}
-    
-	log.Println("os.Args[1] = ", os.Args[1])
-
- 	_, err = conn.Write([]byte(os.Args[1]))
+	defer conn.Close()
+_, err = conn.Write([]byte(string(kryptertMelding)))
 	if err != nil {
 		log.Fatal(err)
 	}
-	buf := make([]byte, 1024)
+buf := make([]byte, 1024)
 	n, err := conn.Read(buf)
 	if err != nil {
 		log.Fatal(err)
-	} 
-	response := string(buf[:n])
-	log.Printf("reply from proxy: %s", response)
+	}
+response := string(buf[:n])
+	log.Printf("Reply from server: %s", response)
 }
